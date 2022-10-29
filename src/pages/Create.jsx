df2880d38c685cb4cf283f2d4282174f1ac8f6f8
Riadh mouamnia <riad.sodmg@gmail.com>
2022-10-30 00:49:34 +0100

    added Button component and Container

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
@@ -12,6 +14,14 @@ export default function Create() {
       >
         Create page
       </Typography>
-    </div>
+      <Button
+        onClick={() => console.log("clicked")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+    </Container>
   );
 }
