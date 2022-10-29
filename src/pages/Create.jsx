dd97e66d2d1bda275e330e0dc390eac6eaee49e7
Riadh mouamnia <riad.sodmg@gmail.com>
2022-10-29 21:16:56 +0100

    Added typography component and edit styles

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create page
+      </Typography>
+    </div>
+  );
 }
