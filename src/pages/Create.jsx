682b0f43705d21df97ab1b735eeafe81f24449bd
Riadh mouamnia <riad.sodmg@gmail.com>
2022-10-30 14:08:41 +0100

    installed Icons and add arrow icon to button

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function Create() {
   return (
@@ -19,6 +20,7 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
